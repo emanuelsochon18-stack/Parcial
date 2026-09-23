8a4d35e124d5e84d545a5c43263594dea03f0050
emanuelsochon18-stack <emanuelsochon18@gmail.com>
2026-09-23 08:39:18 -0600

    Update script.js

--- Tercer Nivel/script.js
+++ Tercer Nivel/script.js
@@ -119,7 +119,7 @@ const componentes = [
 
     {
         nombre: "Capacitor Cerámico",
-        real: "../Primer Nivel/img/preguntas/ceramico.png",
+        real: "../Primer Nivel/img/preguntas/Ceramico.png",
         simbolo: "../Segundo Nivel/img/simbologia/ceramico.png"
     },
 
